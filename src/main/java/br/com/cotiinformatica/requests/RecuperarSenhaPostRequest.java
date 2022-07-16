e03496b86717011fbbf26cc98b189eee20e4506d
AlbdizRos <albertitomusical@gmail.com>
2022-07-16 09:27:46 -0300

    Cadastro, autenticação e recuperação de senha de usuários

---
+++ src/main/java/br/com/cotiinformatica/requests/RecuperarSenhaPostRequest.java
@@ -0,0 +1,15 @@
+package br.com.cotiinformatica.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class RecuperarSenhaPostRequest {
+
+	private String email;
+	
+}
+
+
+
